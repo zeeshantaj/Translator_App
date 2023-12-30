464357eb7a203f82661d17b6e19377fcc589297e
zeeshantaj <zeeshantaj.zt@gmail.com>
2023-12-30 23:45:59 +0500

    first4 commit

--- app/src/main/java/com/example/langauage_translator_app/MainActivity.java
+++ app/src/main/java/com/example/langauage_translator_app/MainActivity.java
@@ -4,16 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.RotateAnimation;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslateLanguage;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslator;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +30,14 @@ public class MainActivity extends AppCompatActivity {
     private TextView translatedTxt;
     private EditText editText;
     private Button translateBtn;
+    private Spinner toSpinner, fromSpinner;
+
+    private String[] fromLanguage = {"from", "English", "Urdu", "Hindi", "Arabic", "French", "Japanese", "Chines", "Italian", "German", "Russian"};
+    private String[] toLanguage = {"to", "English", "Urdu", "Hindi", "Arabic", "French", "Japanese", "Chines", "Italian", "German", "Russian"};
+
+    private static final int REQUEST_PERMISSION_CODE = 1;
+    private int languageCode, fromLanguageCode, toLanguageCode = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +47,10 @@ public class MainActivity extends AppCompatActivity {
         translatedTxt = findViewById(R.id.translatedTxt);
         editText = findViewById(R.id.editText);
         translateBtn = findViewById(R.id.translateBtn);
+        toSpinner = findViewById(R.id.toSpinner);
+        fromSpinner = findViewById(R.id.fromSpinner);
+
+
         arrowBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -39,32 +59,91 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         translateBtn.setOnClickListener(v -> {
-            String textToTranslate = editText.getText().toString().trim();
-            translateText(textToTranslate);
+
         });
+        toSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                toLanguageCode = getLanguageCode(toLanguage[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        ArrayAdapter toAdapter = new ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+        toAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        toSpinner.setAdapter(toAdapter);
+        fromSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                fromLanguageCode = getLanguageCode(fromLanguage[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        ArrayAdapter fromAdapter = new ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item);
+        fromAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        fromSpinner.setAdapter(fromAdapter);
+
+    }
+
+    //"English","Urdu","Hindi","Arabic","French","Japanese","Chines","Italian","German","Russian"
+    private int getLanguageCode(String language) {
+        int languageCode = 0;
+        switch (language) {
+            case "English":
+                languageCode = FirebaseTranslateLanguage.EN;
+                break;
+            case "Urdu":
+                languageCode = FirebaseTranslateLanguage.UR;
+                break;
+            case "Hindi":
+                languageCode = FirebaseTranslateLanguage.HI;
+                break;
+            case "Arabic":
+                languageCode = FirebaseTranslateLanguage.HI;
+                break;
+            case "French":
+                languageCode = FirebaseTranslateLanguage.HI;
+                break;
+            case "Japanese":
+                languageCode = FirebaseTranslateLanguage.HI;
+                break;
+            case "Chines":
+                languageCode = FirebaseTranslateLanguage.HI;
+                break;
+            case "Italian":
+                languageCode = FirebaseTranslateLanguage.HI;
+                break;
+            case "German":
+                languageCode = FirebaseTranslateLanguage.HI;
+                break;
+            case "Russian":
+                languageCode = FirebaseTranslateLanguage.HI;
+                break;
+
+
+
+        }
 
 
     }
+
     private void startAnimation() {
 
-        ObjectAnimator animator = ObjectAnimator.ofFloat(arrowBtn,"rotation",0f,360f);
+        ObjectAnimator animator = ObjectAnimator.ofFloat(arrowBtn, "rotation", 0f, 360f);
         animator.setDuration(1000);
         AnimatorSet animatorSet = new AnimatorSet();
         animatorSet.playTogether(animator);
         animatorSet.start();
 
     }
-    private String translateText(String textToTranslate) {
-        String translatedText = "";
-
-        // Translate the text from English to Urdu
-//        Translate translate = TranslateOptions.getDefaultInstance().getService();
-//        Translation translation = translate.translate(textToTranslate,
-//                Translate.TranslateOption.targetLanguage("ur"));
-//
-//        translatedText = translation.getTranslatedText();
-        return translatedText;
-    }
-
 
 }
